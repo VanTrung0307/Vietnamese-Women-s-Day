a7ed57d74180e5bdc7e0257bbc784329e9329ca1
Trung Văn <74370274+VanTrung0307@users.noreply.github.com>
2023-10-20 06:24:18 +0700

    wonan day

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em Bé ❤️'
+const giftUrl = 'https://www.facebook.com/vantrun0307'
+const eventName = 'Chúc Mừng 20-10'
+const titleCard = 'Tặng Em Bé'
+const contentCard =
+  'Chúc vợ iu của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau'
+
+const giftImage = 'congchua.jpg'
+const base64 = ''
+const giftImageBase64 = 'data:image/png;base64, ' + base64
